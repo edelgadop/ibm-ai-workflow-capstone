85173d818e6388a5f2de5e07eda0599abf610077
Emilio Delgado Pascual <emdp.91@gmail.com>
2021-08-30 09:08:22 +0200

    Init branch

--- source/ingestion/preprocessing.py
+++ source/ingestion/preprocessing.py
@@ -1,9 +1,8 @@
 """
-ingestion_pipeline
+preprocessing
 ===============================================================================
 
-Module used to gather and load the source datasets used in the Capstone Project
-for the AI Workflow specialization (IBM, Coursera)
+Transformations for the input data
 
 Author: Emilio Delgado Pascual
 """
